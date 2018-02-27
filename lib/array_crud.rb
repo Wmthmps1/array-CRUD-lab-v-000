d1ee2672ef74b012095e1813d66c751053675619
William Thompson <wmthmps1@gmail.com>
2018-02-27 01:35:53 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,8 @@ def remove_element_from_start_of_array(array)
   Sonic_Characters.shift
 end
 
-index_number=2
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+index_number = 2
 
 def retrieve_element_from_index(array, index_number)
   Sonic_Characters[index_number]
