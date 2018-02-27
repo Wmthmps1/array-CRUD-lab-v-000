6c2d8b960f2022fefca39016c25d1849d7f88774
William Thompson <wmthmps1@gmail.com>
2018-02-27 01:36:06 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,7 +28,7 @@ array = ["wow", "I", "am", "really", "learning", "arrays!"]
 index_number = 2
 
 def retrieve_element_from_index(array, index_number)
-  Sonic_Characters[index_number]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
